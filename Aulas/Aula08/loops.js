8925bd579326b22083f44050a3b3bc2c391d09f0
Filipe Muniz <filipembento2009@gmail.com>
2025-10-18 11:56:55 -0300

    Modificando loops.js

--- Aulas/Aula08/loops.js
+++ Aulas/Aula08/loops.js
@@ -6,6 +6,19 @@
 
 // Uso de laço "for"
 
-for (i = 0; i < 1500000; i++) {
+for (i = 0; i < 11; i++) {
     console.log(`[${i}] instância.`)
+}
+
+/*
+    Resultado esperado: 
+    Uma linha de codigo com [i] instância que se repete 11 vezes (0-10) 
+*/
+
+
+// Uso de laço "while"
+
+j = 0;
+while (j < 10) {
+    console.log(`Valor atual de i: [${j}]`)
 }
